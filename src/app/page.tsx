0bfe5ba4e8a57fef54a22c8e8762c2b00a698f3d
LukaD45 <luka.dusko44@gmail.com>
2025-01-02 21:00:24 +0100

    updated classes for button, input from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
+      <Input />
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary" size="default">
+        Secondary
+      </Button>
       <Button variant="destructive" size="default">
-        click me
+        Destructive
+      </Button>
+      <Button variant="ghost" size="default">
+        Ghost
+      </Button>
+      <Button variant="muted" size="default">
+        Muted
+      </Button>
+      <Button variant="outline" size="default">
+        Outline
+      </Button>
+      <Button variant="teritary" size="default">
+        Teritary
       </Button>
     </div>
   );
